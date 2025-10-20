b2f5b37bc93453fbd0de17b4cdafdf8ce545d188
Akshaya Singh <akshaysing975@gmail.com>
2025-10-20 15:12:28 +0530

    Use environment variables for Firebase config

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -2,19 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBtq3YNsBM1HehD_wcwdacWQZKAF-buyGs",
-
-  authDomain: "usermanagement-e78fa.firebaseapp.com",
-
-  projectId: "usermanagement-e78fa",
-
-  storageBucket: "usermanagement-e78fa.firebasestorage.app",
-
-  messagingSenderId: "947928457427",
-
-  appId: "1:947928457427:web:6568583d9a77e44a7b3426",
-
-  measurementId: "G-K52LTHLJHV",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
